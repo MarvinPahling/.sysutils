ff9f57f30d15980f59012ec15053964328e63d44
Marvin Pahling <marvinpahling@gmail.com>
2025-03-24 21:30:59 +0100

    automated backup: 2025-03-24

--- scripts/backup_dotfiles.sh
+++ scripts/backup_dotfiles.sh
@@ -4,14 +4,16 @@ backup_repo() {
   local repo_path=$1
   echo "backing up: $repo_path"
   cd "$repo_path" || exit 1
-  git add .
+  git pull || return
+  git add --all
+  git status
   git commit -m "automated backup: $(date -I)"
   git push origin main
 }
 
-repos=("$HOME/dotfiles" "$HOME/dotfiles/neovim/nvim" "$HOME/dotfiles/sysutils")
+repos=("$HOME/dotfiles" "$HOME/.config/nvim" "$HOME/.sysutils")
 
-for repo in $repos
+for repo in "${repos[@]}"
 do
   backup_repo $repo
 done
