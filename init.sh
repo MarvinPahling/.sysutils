61af82e28e4218fd4a4be5b647dfe06a4b72f12c
Marvin Pahling <marvin.pahling@volkswagen.de>
2025-03-27 18:00:35 +0100

    fixed alias path

--- init.sh
+++ init.sh
@@ -7,5 +7,5 @@ source "$scripts_path/backup-dotfiles.sh"
 # workflows - can be sourced in the current session to automate workflows 
 workflow_path="$HOME/.sysutils/workflows"
 alias daily="source $workflow_path/daily-code/daily-code.sh"
-alias fastbun="source $workflow_path/fast-bun.sh"
-
+alias fastbun="source $workflow_path/fast-bun/fast-bun.sh"
+alias fast-kotlin="source $workflow_path/fast-kotlin/fast-kotlin.sh"
